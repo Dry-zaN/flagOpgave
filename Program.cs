d7e256a23d0f44a46720fdf883e7a8f008561d1f
jhon <jhon@DESKTOP-B67EJTJ>
2023-05-09 11:08:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adds a background color to the text displayed
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("                  ");
+            Console.WriteLine("                  ");
+            Console.WriteLine("                  ");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("                  ");
+            Console.WriteLine("                  ");
+            Console.WriteLine("                  ");
+
+            Console.ReadKey();
+            //Clears the terminal
+            Console.Clear();
+            //Adds a magenta background color to the entire terminal
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ReadKey();
+
+            
+
+
+        }
+    }
+}
